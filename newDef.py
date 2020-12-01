06aa3b169e4a98f56cc20b6c5d3567826d756262
mengjiandong <cmydice@163.com>
2020-12-01 21:25:50 +0800

    newDef

--- newDef.py
+++ newDef.py
@@ -6,4 +6,19 @@ def sum_list(alist):
 print(sum_list)
 my_list = [23, 45, 67, 89, 100]
 my_sum = sum_list(my_list)
+
+"you are my sunshine".split(" ")
+'-'.join['one', 'two','three']
+'abc'.upper()
+'Abc'.lower()
+'hello world'.center(20)
+'hello world'.ljust(10)
+'hello world'.rjust(10)
+'aBc'.swapcase()
+None
+None
+str = 'hello world'
+str[1:5:1]
+ord('a')
+'Tom smile, Tom cried, Tom shouted'.replace('Tom', 'Jane')
 print("sum of my list: %d " % (my_sum))
